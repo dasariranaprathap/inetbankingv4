74d6fa013158c201bc56ca979c663a9d877d3c71
nareshqa401 <nareshqa2789@gmail.com>
2023-11-25 12:02:06 +0530

    2nd change

--- src/Gitdemo.java
+++ src/Gitdemo.java
@@ -1,13 +1,44 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Gitdemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void armstrong(int num)
+	
+	{
+		int cube =0;
+		int r;
 		
-		int a =20;
-		int b = 30;
-		System.out.println(a+b);
+		int t;
+		t=num;
+		while(num>0)
+		{
+			r=num%10;
+			
+			num=num/10;
+			cube =cube+(r*r*r);
+			
+		}
+		if(t==cube)
+		{
+			System.out.println("armstroing");
+		}else
+		{
+			System.out.println("not armstrong");
+		}
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		armstrong(15);
+	
 
 	}
 
+	
+	
+	
 }
